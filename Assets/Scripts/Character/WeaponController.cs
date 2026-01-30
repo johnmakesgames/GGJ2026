af39cd72aa5216a367bcc29034c881324d91b3f4
Tayz <taylorfahy1337@gmail.com>
2026-01-30 23:12:32 +0000

    weapon controller plus other stuff idk

---
+++ Assets/Scripts/Character/WeaponController.cs
@@ -0,0 +1,46 @@
+using System.Net;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WeaponController : MonoBehaviour 
+{
+
+    public Transform weapon;
+    public float maxDistance = 20f;
+    public LayerMask hitMask;
+
+    void Update()
+    {
+        AimAtCursor();
+
+        if (Input.GetMouseButtonDown(0))
+            Shoot();
+    }
+
+    void AimAtCursor()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mousePos - weapon.position).normalized;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        weapon.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    void Shoot()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mousePos - weapon.position).normalized;
+
+        RaycastHit2D hit = Physics2D.Raycast(weapon.position, direction, maxDistance, hitMask);
+
+        if (hit.collider != null)
+        {
+            Debug.Log("Hit: " + hit.collider.name);
+        }
+    }
+}
+
+    
+    
+
+
