ee95597ff2a9faac7ceabb5834339deb51a959ff
johnmakesgames <john.whiles@hotmail.com>
2026-01-30 19:17:53 +0000

    Very simple character controller input setup

--- Assets/Scripts/Character/CharacterController.cs
+++ Assets/Scripts/Character/CharacterController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterController : MonoBehaviour
 {
@@ -6,35 +7,43 @@ public class CharacterController : MonoBehaviour
     float movementSpeed;
 
     [SerializeField]
+    float sprintModifier;
+
+    [SerializeField]
+    float sneakModifier;
+
+    [SerializeField]
     Vector3 movementDirection;
+
+    // Movement inputs
+    InputAction moveAction;
+    InputAction sprintAction;
+    InputAction sneakAction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        moveAction = InputSystem.actions.FindAction("Move");
+        sprintAction = InputSystem.actions.FindAction("Sprint");
+        sneakAction = InputSystem.actions.FindAction("Crouch");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
+        float thisFrameMoveSpeed = movementSpeed;
+        movementDirection = moveAction.ReadValue<Vector2>();
 
+        if (sprintAction.ReadValue<float>() > 0)
+        {
+            thisFrameMoveSpeed += sprintModifier;
+        }
+        
+        if (sneakAction.ReadValue<float>() > 0)
+        {
+            thisFrameMoveSpeed += sneakModifier;
         }
 
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-
-        }
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-
-        }
-
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-
-        }
+        this.transform.position += (movementDirection * thisFrameMoveSpeed * Time.deltaTime);
     }
 }
