6e04f4c5f61dcef729b146882070a751097c12fb
Jacob Green <7964268+Jakey113G@users.noreply.github.com>
2026-01-30 20:38:06 +0000

    Add red and blue colour to health and oxygen bars

--- Assets/Scripts/UI/OxygenBarControl.cs
+++ Assets/Scripts/UI/OxygenBarControl.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class OxygenBarControl : MonoBehaviour
 {
     public Slider OxygenSlider;
+    public Slider HealthSlider;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
