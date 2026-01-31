2b6a36aee5959cf4d8dfd86ed6a1d210896b3f48
Jacob Green <7964268+Jakey113G@users.noreply.github.com>
2026-01-31 14:51:47 +0000

    World Text

--- Assets/Scripts/UI/WorldUIEntityComponent.cs
+++ Assets/Scripts/UI/WorldUIEntityComponent.cs
@@ -15,7 +15,6 @@ public class WorldUIEntityComponent : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
