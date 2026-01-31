f70abb5659e599d1929bc37fe06e7a2b2ff275fe
Tayz <taylorfahy1337@gmail.com>
2026-01-31 20:25:09 +0000

    weapon code still borkd

--- Assets/Scripts/Character/WeaponController.cs
+++ Assets/Scripts/Character/WeaponController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,12 +8,15 @@ public class WeaponController : MonoBehaviour
     public Transform weapon;
     public float maxDistance = 20f;
     public LayerMask hitMask;
+    
+    Collider myCollider;
 
     private InputAction shootAction;
 
     void Start()
     {
         shootAction = InputSystem.actions.FindAction("Attack");
+        myCollider = weapon.GetComponent<Collider>();
     }
 
     void Update()
@@ -25,10 +30,27 @@ public class WeaponController : MonoBehaviour
 
     void Shoot()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        Plane aimPlane  = new Plane(Vector3.up, Vector3.zero);
         
-        Vector3 rayDirection = (mousePos - weapon.position).normalized;
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
 
-        Debug.DrawRay(weapon.position, rayDirection, Color.green);
+        float entryPoint;
+        Vector3 aimPoint;
+
+        if (aimPlane.Raycast(ray, out entryPoint))
+        {
+            aimPoint = ray.GetPoint(entryPoint);
+        }
+        else
+        {
+            aimPoint = transform.position + transform.forward * 10f;
+        }
+        
+        Vector3 origin = weapon.transform.position;
+        Vector3 direction = (aimPoint - origin).normalized;
+        
+        
+        
+        Debug.DrawRay(origin, direction * maxDistance, Color.green);
     }
 }
