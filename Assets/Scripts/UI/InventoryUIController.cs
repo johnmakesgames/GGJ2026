830ef68c17967b25c133886d4d2fc36b34c04ec1
Jacob Green <7964268+Jakey113G@users.noreply.github.com>
2026-02-01 12:47:55 +0000

    Inventory use and delete support

--- Assets/Scripts/UI/InventoryUIController.cs
+++ Assets/Scripts/UI/InventoryUIController.cs
@@ -14,6 +14,10 @@ public class InventoryUIController : MonoBehaviour
     InventoryItemHandler m_CurrentItem = null;
     InventorySelector m_InventorySelector = null;
 
+    public GameObject VisualIndicator = null;
+    public GameObject ConsumeItem = null;
+    public GameObject TrashItem = null;
+
     bool Closing = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -41,23 +45,74 @@ public class InventoryUIController : MonoBehaviour
 
             m_InventorySelector = PlayerInventory.TryConsumeInventorySelection();
         }
+
+        RefreshVisualSelector();
+        RefreshCanUseItem(null);
+        RefreshCanTrashItem(null);
     }
 
     public void Selected(InventoryItemHandler handler)
     {
         m_CurrentItem = handler;
+        RefreshVisualSelector();
         RefreshOptions();
     }
 
+    void RefreshVisualSelector()
+    {
+        if (VisualIndicator)
+        {
+            if (m_CurrentItem)
+            {
+                VisualIndicator.transform.position = m_CurrentItem.gameObject.transform.position;
+            }
+            VisualIndicator.SetActive(m_CurrentItem != null && m_CurrentItem.IsViableItem());
+        }
+    }
     void RefreshOptions()
     {
-        if(m_InventorySelector)
+        if (m_InventorySelector)
         {
             m_InventorySelector.SetItemToShow(m_CurrentItem ? m_CurrentItem.m_Tag : ItemTag.COUNT);
             CloseInventoryScene();
         }
+        else
+        {
+            RefreshCanUseItem(m_CurrentItem);
+            RefreshCanTrashItem(m_CurrentItem);
+        }
+    }
 
-        //TODO Present options on what to do with the item
+    private void RefreshCanTrashItem(InventoryItemHandler currentItem)
+    {
+        if (TrashItem)
+        {
+            bool canTrash = currentItem ? PlayerInventory.CanDelete(currentItem.m_Tag) : false;
+            TrashItem.SetActive(canTrash);
+        }
+    }
+
+    private void RefreshCanUseItem(InventoryItemHandler currentItem)
+    {
+        if (ConsumeItem)
+        {
+            bool canInvoke = currentItem ? PlayerInventory.HasAction(currentItem.m_Tag) : false;
+            ConsumeItem.SetActive(canInvoke);
+        }
+    }
+
+    public void Click_ConsumeItem()
+    {
+        PlayerInventory.UseItem(m_CurrentItem.m_Tag);
+        m_CurrentItem.Hide();
+        RefreshVisualSelector();
+    }
+
+    public void Click_TrashItem()
+    {
+        PlayerInventory.RemoveItem(m_CurrentItem.m_Tag);
+        m_CurrentItem.Hide();
+        RefreshVisualSelector();
     }
 
     // Update is called once per frame
