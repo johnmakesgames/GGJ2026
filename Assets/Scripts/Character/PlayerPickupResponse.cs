a7d22e126b1df7d4c2c438c5af756c7246898a30
Jacob Green <7964268+Jakey113G@users.noreply.github.com>
2026-02-01 14:49:43 +0000

    Null safe

--- Assets/Scripts/Character/PlayerPickupResponse.cs
+++ Assets/Scripts/Character/PlayerPickupResponse.cs
@@ -19,35 +19,35 @@ public class PlayerPickupResponse : MonoBehaviour
         {
             case PickupTypes.Health:
                 playerStats.CurrentHealth += pickupAmount;
-                worldUIController.ShowStatGained($"+{pickupAmount}" + " Health Gained", this.gameObject, WorldUIController.WorldUIType.HealthGame);
+                worldUIController?.ShowStatGained($"+{pickupAmount}" + " Health Gained", this.gameObject, WorldUIController.WorldUIType.HealthGame);
                 break;
             case PickupTypes.Oxygen:
                 playerStats.CurrentOxygen += pickupAmount;
-                worldUIController.ShowStatGained("Oxygen Gained", this.gameObject, WorldUIController.WorldUIType.OxygenGain);
+                worldUIController?.ShowStatGained("Oxygen Gained", this.gameObject, WorldUIController.WorldUIType.OxygenGain);
                 break;
             case PickupTypes.Ammo:
                 playerStats.AddAmmoToStockpile(pickupAmount * 5);
                 int ammoGained = pickupAmount * 5;
-                worldUIController.ShowStatGained($"{ammoGained}" + " Ammo", this.gameObject, WorldUIController.WorldUIType.Scavenge);
+                worldUIController?.ShowStatGained($"{ammoGained}" + " Ammo", this.gameObject, WorldUIController.WorldUIType.Scavenge);
                 break;
             case PickupTypes.FullHeal:
                 playerStats.IncreaseMaxHealth(0, true);
-                worldUIController.ShowStatGained("Refilled Health", this.gameObject, WorldUIController.WorldUIType.HealthGame);
+                worldUIController?.ShowStatGained("Refilled Health", this.gameObject, WorldUIController.WorldUIType.HealthGame);
                 break;
             case PickupTypes.FullOxygen:
                 playerStats.IncreaseMaxOxygen(0, true);
-                worldUIController.ShowStatGained("Refilled Oxygen", this.gameObject, WorldUIController.WorldUIType.OxygenGain);
+                worldUIController?.ShowStatGained("Refilled Oxygen", this.gameObject, WorldUIController.WorldUIType.OxygenGain);
                 break;
             case PickupTypes.FullAmmo:
                 Debug.LogWarning("No implementation for Full Ammmo pickup");
                 break;
             case PickupTypes.IncreaseMaxHealth:
                 playerStats.IncreaseMaxHealth(pickupAmount, false);
-                worldUIController.ShowStatGained("Increased Max Health", this.gameObject, WorldUIController.WorldUIType.HealthGame);
+                worldUIController?.ShowStatGained("Increased Max Health", this.gameObject, WorldUIController.WorldUIType.HealthGame);
                 break;
             case PickupTypes.IncreaseMaxOxygen:
                 playerStats.IncreaseMaxOxygen(pickupAmount, false);
-                worldUIController.ShowStatGained("Increased Max Oxygen", this.gameObject, WorldUIController.WorldUIType.OxygenGain);
+                worldUIController?.ShowStatGained("Increased Max Oxygen", this.gameObject, WorldUIController.WorldUIType.OxygenGain);
                 break;
             case PickupTypes.IncreaseMaxAmmo:
                 Debug.LogWarning("No implementation for increase max Ammmo pickup");
@@ -55,7 +55,7 @@ public class PlayerPickupResponse : MonoBehaviour
             case PickupTypes.Item:
                 if(this.GetComponent<PlayerInventory>().TryAddItem(item))
                 {
-                    worldUIController.ShowItemPickedup(item, this.gameObject);
+                    worldUIController?.ShowItemPickedup(item, this.gameObject);
                 }
                 break;
             default:
