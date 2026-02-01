7457dbf9766e869a18f48dfe23793cd6fd7ac228
johnmakesgames <john.whiles@hotmail.com>
2026-02-01 10:28:52 +0000

    Added credits prefab to all menu screens

--- Assets/Scripts/UI/Menus/MainMenu.cs
+++ Assets/Scripts/UI/Menus/MainMenu.cs
@@ -3,6 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    GameObject creditsPanel;
+    GameObject settingsPanel;
+
+    private void Start()
+    {
+        creditsPanel = GameObject.Find("CreditsPanel");
+        if (creditsPanel == null )
+        {
+            creditsPanel.SetActive(false);
+        }
+
+        settingsPanel = GameObject.Find("SettingsPanel");
+        if (settingsPanel != null )
+        {
+            settingsPanel.SetActive(false);
+        }
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("BaseScene");
@@ -16,12 +34,18 @@ public class MainMenu : MonoBehaviour
 
     public void Settings()
     {
-
+        if (settingsPanel != null)
+        {
+            settingsPanel.SetActive(!settingsPanel.activeInHierarchy);
+        }
     }
 
     public void Credits()
     {
-
+        if (creditsPanel != null)
+        {
+            creditsPanel.SetActive(!creditsPanel.activeInHierarchy);
+        }
     }
 
     public void Quit()
